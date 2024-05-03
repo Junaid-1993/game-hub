444f096ebe8a82cc2102230be8c25cc68ca881be
junaid-1993 <junaidali9001@gmail.com>
2024-05-03 11:40:14 +0500

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 function App() {
   // Initially no genre is selected.
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  // Initially no platform is selected.
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
   return (
     <Grid
@@ -30,8 +33,8 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area={"main"}>
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)} />
+        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
   );
