3a6ebbe5a01f28baa2c6eefa03c957f85c0c8bf1
junaid-1993 <junaidali9001@gmail.com>
2024-05-17 11:49:41 +0500

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,11 +9,13 @@ const apiClient = new APIClient<Game>("/games");
 export interface Game {
   id: number;
   name: string;
+  slug: string;
   background_image: string;
   // Array of objects and each object has a property called 'platform' which is of type 'Platform'.
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
+  description_raw: string;
 }
 
 const useGames = () => {
